1c32d9e2c858c3619830b564bc43473542cf1961
keemnhy <keemnhy@gmail.com>
2025-10-10 17:21:55 +0900

    포인트배너만들고,리얼리뷰 진행 중

--- src/components/Real_review.vue
+++ src/components/Real_review.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-리얼리뷰
+  <div class="real-review">
+    <div class="inner">
+      <div class="title-box">
+        <div class="stars">★★★★</div>
+        <h2 class="title">사장님 리얼리뷰</h2>
+      </div>
+      <div class="review-list">
+        <div class="review-item" v-for="(review,index) in review" :key="index"></div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
